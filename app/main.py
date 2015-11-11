28c55e797d9e13149ce50f1b0a74e39b3606fa1a
iThink <yqsseu@163.com>
2015-11-11 21:28:53 +0800

    change the readmessage state bug

--- app/main.py
+++ app/main.py
@@ -470,61 +470,36 @@ def editpreferinformation():
 
 @app.route("/getprofile",methods=['GET','POST'])
 def getprofile():
-		try:
-			token = request.json['token']
-			u=getuserinformation(token)
-	 		if u!=None:
-				state = 'successful'
-				reason = ''
-				username = u.username if u.username!=None else ''
-				token = u.token if u.token!=None else ''    
-				school = u.school if u.school!=None else '' 				
-				degree = u.degree if u.degree!=None else ''
-				department = u.department if u.department!=None else ''
-				enrollment = u.enrollment if u.enrollment!=None else ''
-				name = u.name if u.name!=None else ''
-				gender = u.gender if u.gender!=None else ''
-				birthday = u.birthday if u.birthday!=None else ''
-				hobby = u.hobby if u.hobby!=None else ''
-				preference = u.preference if u.preference!=None else '' 
-				phone = u.phone if u.phone!=None else ''
-				wechat = u.wechat if u.wechat != None else ''
-				qq = u.qq if u.qq != None else ''
-				hometown = u.hometown if u.hometown != None else ''
-				id = u.id if u.id!=None else ''
-				qianshoudongda = u.qianshoudongda if u.qianshoudongda!=None else ''
-				autumn1 = u.autumn1 if u.autumn1!=None else ''
-				autumn2 = u.autumn2 if u.autumn2!=None else ''
-				autumn3 = u.autumn3 if u.autumn3!=None else ''
-			else:
-				state = 'fail'
-				reason = '用户不存在'
-				username = 'Nouser'
-				token=''
-				school=''
-				degree=''
-				department = ''
-				enrollment = ''
-				name = ''
-				gender = ''
-				birthday = ''
-				hobby = ''
-				preference = ''
-				phone = ''
-				wechat = ''
-				qq =''
-				hometown = ''
-				id = ''
-				qianshoudongda =''
-				autumn1 = ''
-				autumn2=''
-				autumn3 = ''
-
-
-		except Exception, e:
+	try:
+		token = request.json['token']
+		u=getuserinformation(token)
+ 		if u!=None:
+			state = 'successful'
+			reason = ''
+			username = u.username if u.username!=None else ''
+			token = u.token if u.token!=None else ''    
+			school = u.school if u.school!=None else '' 				
+			degree = u.degree if u.degree!=None else ''
+			department = u.department if u.department!=None else ''
+			enrollment = u.enrollment if u.enrollment!=None else ''
+			name = u.name if u.name!=None else ''
+			gender = u.gender if u.gender!=None else ''
+			birthday = u.birthday if u.birthday!=None else ''
+			hobby = u.hobby if u.hobby!=None else ''
+			preference = u.preference if u.preference!=None else '' 
+			phone = u.phone if u.phone!=None else ''
+			wechat = u.wechat if u.wechat != None else ''
+			qq = u.qq if u.qq != None else ''
+			hometown = u.hometown if u.hometown != None else ''
+			id = u.id if u.id!=None else ''
+			qianshoudongda = u.qianshoudongda if u.qianshoudongda!=None else ''
+			autumn1 = u.autumn1 if u.autumn1!=None else ''
+			autumn2 = u.autumn2 if u.autumn2!=None else ''
+			autumn3 = u.autumn3 if u.autumn3!=None else ''
+		else:
 			state = 'fail'
-			reason = '异常'	
-			username='e'
+			reason = '用户不存在'
+			username = 'Nouser'
 			token=''
 			school=''
 			degree=''
@@ -537,35 +512,60 @@ def getprofile():
 			preference = ''
 			phone = ''
 			wechat = ''
-			qq = ''
+			qq =''
 			hometown = ''
 			id = ''
-			qianshoudongda=''
+			qianshoudongda =''
 			autumn1 = ''
-			autumn2 = ''
+			autumn2=''
 			autumn3 = ''
 
-			
 
-		response = jsonify({'username':username,
-							'token':token,
-							'state':state,
-							'reason':reason,
-		 	                'school':school,
-		 	                'degree':degree,
-		 	                'department':department,
-		 	                'enrollment':enrollment,
-		 	                'name':name,
-		 	                'gender':gender,
-		 	                'birthday':birthday,
-		 	                'preference':preference,
-		 	                'hobby':hobby,
-		 	                'phone':phone,
-		 	                'wechat':wechat,
-		 	                'qq':qq,
-		 	                'hometown':hometown,
-		 	                'id':id})
-		return response
+	except Exception, e:
+		state = 'fail'
+		reason = '异常'	
+		username='e'
+		token=''
+		school=''
+		degree=''
+		department = ''
+		enrollment = ''
+		name = ''
+		gender = ''
+		birthday = ''
+		hobby = ''
+		preference = ''
+		phone = ''
+		wechat = ''
+		qq = ''
+		hometown = ''
+		id = ''
+		qianshoudongda=''
+		autumn1 = ''
+		autumn2 = ''
+		autumn3 = ''
+
+		
+
+	response = jsonify({'username':username,
+						'token':token,
+						'state':state,
+						'reason':reason,
+	 	                'school':school,
+	 	                'degree':degree,
+	 	                'department':department,
+	 	                'enrollment':enrollment,
+	 	                'name':name,
+	 	                'gender':gender,
+	 	                'birthday':birthday,
+	 	                'preference':preference,
+	 	                'hobby':hobby,
+	 	                'phone':phone,
+	 	                'wechat':wechat,
+	 	                'qq':qq,
+	 	                'hometown':hometown,
+	 	                'id':id})
+	return response
 
 
 @app.route("/getprofilebyid",methods=['GET','POST'])
@@ -884,12 +884,11 @@ def readmessage():
 		u = getuserinformation(token)
 		m = getMessagebyid(id)
 		if u != None:
-			print 'sss'
-			m.state = 0
-			m.add()			
+			if m.RecId == u.id:
+				m.state = 0
+				m.add()			
 			state = 'successful'
 			reason = ''
-
 		else:
 			state = 'fail'
 			reason = 'no user'
