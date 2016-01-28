b2f4232e24f907d19088697918bb9ba5c3fa3203
iThink <yqsseu@163.com>
2016-01-28 09:09:43 +0800

    morning card.jpg

--- app/script/transfer.py
+++ app/script/transfer.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import shutil
 import os,stat
+from PIL import Image
 from tmodels import *
 
 """define checkdbNone"""
 def checkdb(dbNone):
 	return dbNone if dbNone!=None else ''
 
-
 cnt1 = 0
 cnt2 = 0
 cnt3 = 0
 cnt4 = 0
-for id in xrange(1,1000):
+for id in xrange(1,1050):
 	#数据库操作
 	avatartmp = getavatarvoicebyuserid(id)
 	if avatartmp!=None:
@@ -22,12 +22,13 @@ for id in xrange(1,1000):
 		try:
 			avatartmp.name = avatartmp.author.name if avatartmp.author.name!=None else ''
 			avatartmp.gender = avatartmp.author.gender if avatartmp.author.gender!=None else ''
-			print " in try"
+			#print " in try"
 		except Exception, e:
 			cnt4 = cnt4 + 1
 			print "None author"
 		avatartmp.add()
-		print str(id) + ":" + checkdb(avatartmp.author.name) +"," + checkdb(avatartmp.name) + checkdb(avatartmp.author.gender)+"," + checkdb(avatartmp.gender) + "have done!"
+		print str(id) + 'havedone'
+		#print str(id) + ":" + checkdb(avatartmp.author.name) +"," + checkdb(avatartmp.name) + checkdb(avatartmp.author.gender)+"," + checkdb(avatartmp.gender) + "have done!"
 	else:
 		cnt2 = cnt2 + 1
 		avatarnumber = 1
@@ -42,6 +43,14 @@ for id in xrange(1,1000):
 			#第一次上传头像，新增
 			tmp = avatarvoice(userid = id,avatarurl = avatarurl,avatar_number = avatarnumber)
 			tmp.add()
+			#生成中等缩略图
+			fp = Image.open(dst)
+			fp.thumbnail((500,500))
+			fp.save(dst + '_card.jpg')
+			#生成缩略图
+			fp = Image.open(dst)
+			fp.thumbnail((200,200))
+			fp.save(dst + '_thumbnail.jpg')
 print "have done = " + str(cnt1)
 print "no author = " + str(cnt4)
 print "no avatar = " + str(cnt2)
