8733491c52a54c970c09ce1d2b22c3524a917056
iThink <yqsseu@163.com>
2016-02-26 21:10:47 +0800

    no token when check apk version

--- app/androidApk.py
+++ app/androidApk.py
@@ -17,37 +17,27 @@ def checkdb(dbNone):
 @androidapk_route.route("/checkapkversion",methods=['POST'])
 def checkapkversion():
 	try:
-		token = request.json['token']
 		v1_now = string.atoi(str(request.json.get('v1','1')))
 		v2_now = string.atoi(str(request.json.get('v2','0')))
 		v3_now = string.atoi(str(request.json.get('v3','0')))
-		u = getuserinformation(token)
-		if u is not None:
-			wemeapk = androidversion.query.filter_by(disable = False).order_by(androidversion.timestamp.desc()).first()
-			if wemeapk!=None:
-				v1_newest = wemeapk.v1
-				v2_newest = wemeapk.v2
-				v3_newest = wemeapk.v3
-				numNow = v1_now*10000 + v2_now*100 + v3_now
-				numNewest = v1_newest*10000 + v2_newest*100 + v3_newest
-				print "now" + str(numNow)
-				print "newest" + str(numNewest)
-				if numNow < numNewest:
-					update_flag = "yes"
-					apkurl = wemeapk.wemeurl
-				else:
-					update_flag = "no"
-					apkurl = ""
-				state = "successful"
-				reason = ""
+		wemeapk = androidversion.query.filter_by(disable = False).order_by(androidversion.timestamp.desc()).first()
+		if wemeapk!=None:
+			v1_newest = wemeapk.v1
+			v2_newest = wemeapk.v2
+			v3_newest = wemeapk.v3
+			numNow = v1_now*10000 + v2_now*100 + v3_now
+			numNewest = v1_newest*10000 + v2_newest*100 + v3_newest
+			if numNow < numNewest:
+				update_flag = "yes"
+				apkurl = wemeapk.wemeurl
 			else:
-				state = 'fail'
-				reason = 'Not apk in server'
 				update_flag = "no"
 				apkurl = ""
+			state = "successful"
+			reason = ""
 		else:
 			state = 'fail'
-			reason = 'no user'
+			reason = 'Not apk in server'
 			update_flag = "no"
 			apkurl = ""
 	except Exception, e:	
