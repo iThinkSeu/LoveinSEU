de41d94c2e5f5f0e9e63067668f70a72b8c403e6
iThink <yqsseu@163.com>
2016-02-26 21:04:03 +0800

    topiclist likenumber

--- app/communityroute.py
+++ app/communityroute.py
@@ -234,7 +234,7 @@ def gettopiclist():
 			topiclist = gettopiclistdb()
 			result = []
 			for i in range(len(topiclist)):
-				posts = post.query.filter_by(topicid=topiclist[i].id).all()
+				posts = post.query.filter_by(disable = 0).filter_by(topicid=topiclist[i].id).all()
 				hotindex = sum([p.likenumber + p.commentnumber for p in posts])
 				output = {"id":topiclist[i].id,"theme":topiclist[i].theme,"imageurl":topiclist[i].imageurl,"note":topiclist[i].note,"number":hotindex}
 				result.append(output)
