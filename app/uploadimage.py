56ebdd51437ee53d8c3d00120e3ecfcbd0428317
LiewLi <liewli@163.com>
2016-04-09 14:04:03 +0800

    better image thumbnail support

--- app/uploadimage.py
+++ app/uploadimage.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from flask import request,jsonify,json
 from models import *
 import os, stat
-from PIL import Image
+from PIL import Image, ExifTags, ImageOps
 import string
 #import Image
 import shutil
@@ -11,6 +11,31 @@ import uuid
 
 upload_image = Blueprint('upload_image', __name__)
 
+
+
+def thumnail_enhanced(image, width, height):
+	try:
+		if hasattr(image, '_getexif'):
+			for orientation in ExifTags.TAGS.keys():
+				if ExifTags.TAGS[orientation] == 'Orientation':
+					break 
+			e = image._getexif()
+			if e is not None:
+				exif = dict(e.items())
+				orientation = exif.get(orientation, None)
+				if orientation is None: return
+
+				if orientation == 3: image = image.transpose(Image.ROTATE_180)
+				elif orientation == 6: image = image.transpose(Image.ROTATE_270)
+				elif orientation == 8: image = image.transpose(Image.ROTATE_90)
+
+		# image.thumbnail((width, height), Image.ANTIALIAS)
+		# background = Image.new('RGBA', (width, height), (255, 255, 255, 0))
+		# background.paste(image,((width - image.size[0]) / 2, (height - image.size[1]) / 2))
+		return ImageOps.fit(image, (width, height), Image.ANTIALIAS)
+
+	except:
+		return
 
 
 @upload_image.route("/uploadavatar", methods=['POST'])
@@ -219,16 +244,22 @@ def uploadavatar():
 			os.chmod(dst, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP  | stat.S_IROTH)
 			if type =="0":
 				fp = Image.open(dst)
-				fp.thumbnail((200,200))
-				fp.save(dst + '_thumbnail.jpg')
+				# fp.thumbnail((200,200))
+				fp = thumnail_enhanced(fp, 200, 200)
+				if fp:
+					fp.save(dst + '_thumbnail.jpg')
 			if type == "-4" or type == "-10" or type == "-9" or type == "-2" or type == "-14" or type == "-16":
 				fp = Image.open(dst)
-				fp.thumbnail((200,200))
-				fp.save(dst + '_thumbnail.jpg')
+				# fp.thumbnail((200,200))
+				thumnail_enhanced(fp, 200, 200)
+				if fp:
+					fp.save(dst + '_thumbnail.jpg')
 			if type == "-7":
 				fp = Image.open(dst)
-				fp.thumbnail((400,400))
-				fp.save(dst + '_thumbnail.jpg')
+				# fp.thumbnail((400,400))
+				fp = thumnail_enhanced(fp, 200, 200)
+				if fp:
+					fp.save(dst + '_thumbnail.jpg')
 
 		except Exception, e:
 			print e 
