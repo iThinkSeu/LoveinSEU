d15780583af6440163064558f6015f1a14ee2f55
LiewLi <liewli@163.com>
2016-04-09 23:52:45 +0800

    push enhancement

--- app/push.py
+++ app/push.py
@@ -5,9 +5,9 @@ from models import *
 import string
 from apns import APNs, Payload
 
+client = APNs(use_sandbox=False, cert_file="cert/cert.pem", key_file="cert/key.pem", enhanced = True)
 
 def send_notification(token_hex, payload):
-	client = APNs(use_sandbox=False, cert_file="cert/cert.pem", key_file="cert/key.pem")
 	client.gateway_server.send_notification(token_hex, payload)
 
 def notify_follow_to_user(u, friend):
